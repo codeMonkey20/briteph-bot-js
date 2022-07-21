4fc32346705ac92add659715c4cf69339109da12
Code Monkey <56096375+codeMonkey20@users.noreply.github.com>
2022-07-21 15:51:39 +0800

    integrated to zoho catalyst

---
+++ functions/briteph-bot/src/index.js
@@ -0,0 +1,28 @@
+'use strict';
+require("dotenv").config();
+const { DISCORD_TOKEN } = process.env;
+const { Client, GatewayIntentBits, Collection } = require("discord.js");
+const fs = require("fs");
+
+module.exports = (req, res) => {
+	const client = new Client({
+		intents: [GatewayIntentBits.Guilds],
+	});
+	
+	client.commands = new Collection();
+	client.commandArray = [];
+	
+	const handlerFolder = fs.readdirSync("./src/functions");
+	for (const folder of handlerFolder) {
+		const handlerFiles = fs
+			.readdirSync(`./src/functions/${folder}`)
+			.filter((file) => file.endsWith(".js"));
+		for (const file of handlerFiles) {
+			require(`./functions/${folder}/${file}`)(client);
+		}
+	}
+	
+	client.eventHandler();
+	client.commandHandler();
+	client.login(DISCORD_TOKEN);
+};
