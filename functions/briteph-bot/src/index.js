a8dc2f6717f7b9c6a96c1c31a9cbda80481433a3
Code Monkey <56096375+codeMonkey20@users.noreply.github.com>
2022-07-21 17:36:40 +0800

    tokens and IDs transferred to catalyst database

--- functions/briteph-bot/src/index.js
+++ functions/briteph-bot/src/index.js
@@ -1,28 +1,34 @@
-'use strict';
-require("dotenv").config();
-const { DISCORD_TOKEN } = process.env;
+"use strict";
 const { Client, GatewayIntentBits, Collection } = require("discord.js");
 const fs = require("fs");
+const { default: axios } = require("axios");
 
 module.exports = (req, res) => {
-	const client = new Client({
-		intents: [GatewayIntentBits.Guilds],
-	});
-	
-	client.commands = new Collection();
-	client.commandArray = [];
-	
-	const handlerFolder = fs.readdirSync("./src/functions");
-	for (const folder of handlerFolder) {
-		const handlerFiles = fs
-			.readdirSync(`./src/functions/${folder}`)
-			.filter((file) => file.endsWith(".js"));
-		for (const file of handlerFiles) {
-			require(`./functions/${folder}/${file}`)(client);
-		}
-	}
-	
-	client.eventHandler();
-	client.commandHandler();
-	client.login(DISCORD_TOKEN);
+  axios
+    .get(
+      "https://britephbot-763668882.development.catalystserverless.com/server/fetchKeys/"
+    )
+    .then((response) => {
+			const DISCORD_TOKEN = response.data.TOKEN;
+      const client = new Client({
+        intents: [GatewayIntentBits.Guilds],
+      });
+
+      client.commands = new Collection();
+      client.commandArray = [];
+
+      const handlerFolder = fs.readdirSync("./src/functions");
+      for (const folder of handlerFolder) {
+        const handlerFiles = fs
+          .readdirSync(`./src/functions/${folder}`)
+          .filter((file) => file.endsWith(".js"));
+        for (const file of handlerFiles) {
+          require(`./functions/${folder}/${file}`)(client);
+        }
+      }
+
+      client.eventHandler();
+      client.commandHandler();
+      client.login(DISCORD_TOKEN);
+    });
 };
