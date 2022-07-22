706e914b9884c6bc7fdd63e661d6ac2e3c1334d4
Code Monkey <56096375+codeMonkey20@users.noreply.github.com>
2022-07-22 18:55:05 +0800

    dsu post to catalyst datastore

--- functions/briteph-bot/src/events/client/interactionCreate.js
+++ functions/briteph-bot/src/events/client/interactionCreate.js
@@ -1,3 +1,6 @@
+const { default: axios } = require("axios");
+const { InteractionType } = require("discord.js");
+
 module.exports = {
   name: "interactionCreate",
   async execute(interaction, client) {
@@ -14,6 +17,43 @@ module.exports = {
           content: "Something went wrong on this command...",
           ephemeral: true,
         });
+      }
+    } else if (interaction.type === InteractionType.ModalSubmit) {
+      console.log(interaction);
+
+      // dsu modal
+      if (interaction.fields.fields.find((e) => e.customId === "DSUMessageBox")) {
+        const DSUMessageBox = interaction.fields.getTextInputValue("DSUMessageBox");
+        const nickname = interaction.member.nickname;
+        const today = new Date();
+        const time = `${today.getFullYear()}-${today.getMonth()+1}-${today.getDate()} ${today.toLocaleTimeString('en-GB')}`;
+        const body = [
+          {
+            message: DSUMessageBox,
+            dsu_timestamp: time,
+            employee_name: nickname,
+          },
+        ];
+
+        // post to catalyst DB
+        axios
+          .post(
+            "https://britephbot-763668882.development.catalystserverless.com/server/postDB/dsu",
+            body
+          )
+          .then((response) => {
+
+            // reply to discord
+            const reply = `Daily Stand Up Report by ${nickname}\nTime: ${new Date().toLocaleString("en-GB")}\n\nTask/s:\n${DSUMessageBox}`;
+            interaction.reply({ content: "```" + reply + "```" });
+          });
+      }
+
+      // eod modal
+      else if (interaction.fields.fields.find((e) => e.customId === "EODMessageBox")) {
+        const EODMessageBox =
+          interaction.fields.getTextInputValue("EODMessageBox");
+        await interaction.reply({ content: EODMessageBox });
       }
     }
   },
